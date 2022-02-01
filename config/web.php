e25165de598d40e2c94ef07025546628f4346449
tykatyk <tykatyk@gmail.com>
2022-02-01 21:37:58 +0200

    Add default route

--- config/web.php
+++ config/web.php
@@ -5,6 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    "defaultRoute"=>"message/get-messages",
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
