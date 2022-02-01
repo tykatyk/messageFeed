0bdde4e98fa13145f9524635a75def59a7c610cb
tykatyk <tykatyk@gmail.com>
2022-02-01 21:35:35 +0200

    Refactor app to accept 'application/json'

--- config/web.php
+++ config/web.php
@@ -16,6 +16,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'QYASmtYMNsN1Fe5hDieVF1ozuxWYUYMg',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
