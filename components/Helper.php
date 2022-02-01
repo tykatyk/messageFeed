57b219321bbba4187d99a4b5a58e2912a58b13c0
tykatyk <tykatyk@gmail.com>
2022-02-01 21:39:28 +0200

    Add function to get last minute

--- components/Helper.php
+++ components/Helper.php
@@ -5,6 +5,20 @@ use Yii;
 
 class Helper
 {
+    public static function getLastMinute() : array
+    {
+        $lastMinute = [];
+        $format = "Y-m-d G:i:s";
+        $timezone = new \DateTimeZone(Yii::$app->timeZone);
+        $date = new \DateTime("now", $timezone);
+        $endStr = $date->format($format);
+        $startStr = date($format, strtotime($endStr) - 60);
+
+        $lastMinute["end"] = $endStr;
+        $lastMinute["start"] = $startStr;
+
+        return $lastMinute;
+    }
     public static function prepareSorting($sortingOrder): string
     {
         $sortBy = "";
